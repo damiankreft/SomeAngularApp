4a25e8d86aff43012adc178746b5dbb58e10dacd
Damian Kreft <damiankreft87@gmail.com>
2021-08-07 15:31:14 +0200

    Replace dummy account with account service connection.

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Account } from './account';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  // TODO: Load AccountService uri from a config file.
+  private accountUrl = "http://localhost:5000/accounts/"
+  constructor(private http: HttpClient) {
+  }
 
-  getAccount(email: string): Account { 
-    return { 
-      "email": "account1@example.com",
-      "password": "j3@c8sj#@nf82nx%39rn&J7wnc"
-    } as Account }
+  getAccount(email: string): Observable<Account> {
+    let accountUri = this.accountUrl + email;
+    // return this.http.get<Account>(accountUri)
+    return this.http.get<Account>(this.accountUrl + "test1@example.com")
+  }
 }

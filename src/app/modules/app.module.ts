826c64fe23cb17dba4b994e59ad3c85ab7fd6c80
Damian Kreft <damiankreft87@gmail.com>
2021-08-22 20:08:06 +0200

    Import and configure JwtModule.

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '../components/app/app.component';
 import { AccountRegisterComponent } from '../components/accountComponents/register/register.component';
@@ -9,6 +8,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AccountSignInComponent } from '../components/accountComponents/sign-in/sign-in.component';
 import { NavigationComponent } from '../components/navigation/navigation.component';
+import { JwtModule } from '@auth0/angular-jwt';
+
+export function tokenGetter() {
+  return localStorage.getItem("access_token");
+}
 
 @NgModule({
   declarations: [
@@ -20,6 +24,12 @@ import { NavigationComponent } from '../components/navigation/navigation.compone
   imports: [
     BrowserModule,
     HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ["localhost:5000", "localhost:5001"]
+      }
+    }),
     ReactiveFormsModule,
     AppRoutingModule,
     NgbModule,

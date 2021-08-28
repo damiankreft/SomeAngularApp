2e88fa8b786798a07d379296aae43bd86a76921b
Damian Kreft <damiankreft87@gmail.com>
2021-08-28 11:10:14 +0200

    Implement Role guard.

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountsComponent } from '../components/accountComponents/accounts/accounts.component';
 import { AccountRegisterComponent } from '../components/accountComponents/register/register.component';
 import { AccountSignInComponent } from '../components/accountComponents/sign-in/sign-in.component';
-import { AuthGuardService as AuthGuard }  from '../services/authGuardService/auth-guard.service';
+import { AuthGuardService as AuthGuard, AuthGuardService }  from '../services/authGuardService/auth-guard.service';
+import { RoleGuardService as RoleGuard } from '../services/roleGuardService/role-guard.service';
 
 const routes: Routes = [
-  { path: 'register', component: AccountRegisterComponent },
+  { path: 'register', component: AccountRegisterComponent, canActivate: [AuthGuard] },
   { path: 'sign-in', component: AccountSignInComponent },
-  { path: 'accounts', component: AccountsComponent, canActivate: [AuthGuard] }
+  { path: 'accounts', component: AccountsComponent, canActivate: [RoleGuard], data: {
+    expectedRole: "admin",
+  } },
+  { path: ''}
 ];
 
 @NgModule({

d96011c2adf61f4f4cc7c21f5d8de744fc4009bf
Damian Kreft <damiankreft87@gmail.com>
2021-08-29 21:55:02 +0200

    Remove code bloat - remove HttpParams and HttpHeaders from authService requests.

--- src/app/services/AuthService/auth.service.ts
+++ src/app/services/AuthService/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginDto } from 'src/app/Dto/login-dto';
 import { shareReplay } from 'rxjs/operators';
@@ -11,8 +11,6 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class AuthService {
   private loginUri: string;
 
-  private headers = new HttpHeaders()
-      .append("Control-Allow-Origin", "*");
   constructor(public jwtHelper: JwtHelperService, private http: HttpClient) {
     this.loginUri = environment.apiHost + 'login'
   }
@@ -22,10 +20,7 @@ export class AuthService {
   }
 
   login(loginDto: LoginDto) {
-    const httpParams = new HttpParams()
-      .append("Email", loginDto.email).append("password", loginDto.password);
-
-    return this.http.post<{token: string}>(this.loginUri, loginDto, { headers: this.headers,  params: httpParams } )
+    return this.http.post<{token: string}>(this.loginUri, loginDto)
       .pipe(shareReplay())
       .subscribe((res: any) => {
         localStorage.setItem("access_token", res.token)

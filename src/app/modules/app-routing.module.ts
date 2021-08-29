ef09633c2c1d6c9581e4ed270d16bd0c57bc26a5
Damian Kreft <damiankreft87@gmail.com>
2021-08-29 18:08:02 +0200

    Remove unused import.

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountsComponent } from '../components/accountComponents/accounts/accounts.component';
 import { AccountRegisterComponent } from '../components/accountComponents/register/register.component';
 import { AccountSignInComponent } from '../components/accountComponents/sign-in/sign-in.component';
-import { AuthGuardService as AuthGuard, AuthGuardService }  from '../services/authGuardService/auth-guard.service';
+import { AuthGuardService as AuthGuard }  from '../services/authGuardService/auth-guard.service';
 import { RoleGuardService as RoleGuard } from '../services/roleGuardService/role-guard.service';
 
 const routes: Routes = [

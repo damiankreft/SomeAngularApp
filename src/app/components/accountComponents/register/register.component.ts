c67e31c785a1b2ec78b4f5f73d851ee505f27b7f
Damian Kreft <damiankreft87@gmail.com>
2021-08-30 18:20:34 +0200

    Put all FormControls in FormGroup. Bind to submit button's click event.

--- src/app/components/accountComponents/register/register.component.ts
+++ src/app/components/accountComponents/register/register.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AccountService } from 'src/app/services/accountService/account.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class AccountRegisterComponent implements OnInit {
-  email = new FormControl('');
-  username = new FormControl('');
-  password = new FormControl('');
-  repeatedPassword = new FormControl('');
-  role  = new FormControl();
+export class AccountRegisterComponent {
+  public registerForm: FormGroup;
+  
 
-  constructor() { }
+  constructor(private accountService: AccountService, public formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      email: new FormControl(''),
+      username: new FormControl(''),
+      password: new FormControl(''),
+      repeatedPassword: new FormControl(''),
+      role: new FormControl(),
+    });
+   }
 
-  ngOnInit(): void {
+  onSubmit(): void {
+    console.log(this.registerForm.value);
+    this.accountService.register(this.registerForm.value).subscribe();
   }
 }
